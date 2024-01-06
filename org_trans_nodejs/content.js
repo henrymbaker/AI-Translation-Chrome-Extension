b6dee1c9b6bfee87a49d1810f5a8b70476d9a9b2
Indie Cowan <cowani@wwu.edu>
2024-01-05 21:08:39 -0800

    debug prints

--- org_trans_nodejs/content.js
+++ org_trans_nodejs/content.js
@@ -32,6 +32,7 @@ function clearClass() {
 //var mouseDown = false;
 
 document.addEventListener('mouseup', function (e) {
+    console.log("mouseup");
   // Get the selected text range or create a range manually
   //if mouse is over span element:
   if (e.target.nodeName != 'SPAN') {
@@ -64,6 +65,7 @@ document.addEventListener('mouseup', function (e) {
 });
 
 document.addEventListener('mousedown', function (event) {
+    console.log("mousedown");
   var clickEvent = new MouseEvent('click', {
     bubbles: true,
     cancelable: true,
@@ -82,6 +84,7 @@ document.addEventListener('mousedown', function (event) {
 
 // Mouse listener for any move event on the current document.
 document.addEventListener('mousemove', function (e) {
+    console.log("mousemove");
   if (mouseDown) {
     return;
   }
