9e702ef32859aeff63a14da1084563c002b48fe0
bakerh22 <henrymbaker4@gmail.com>
2024-01-05 17:47:17 -0800

    more progress

--- Mouseover Stuff/content.js
+++ Mouseover Stuff/content.js
@@ -1,9 +1,35 @@
+var MOUSE_VISITED_CLASSNAME = 'crx_mouse_visited';
+var PREV_ELEMENT = null;
+var mouseDown = false;
+
+window.onload = function() {
+  // Select all <p> elements
+  var pElements = document.getElementsByTagName('p');
+
+  for (var i = 0; i < pElements.length; i++) {
+      var pElement = pElements[i];
+      var words = pElement.innerText.split(' ');
+      var spanWrappedWords = words.map(word => '<span>' + word + '</span>');
+      pElement.innerHTML = spanWrappedWords.join(' ');
+  }
+};
+
+
 
 // Unique ID for the className.
-var MOUSE_VISITED_CLASSNAME = 'crx_mouse_visited';
+
+function clearClass() {
+  // Select all elements with MOUSE_VISITED_CLASSNAME
+  var elementsToRemove = document.querySelectorAll('.' + MOUSE_VISITED_CLASSNAME);
+
+  // Loop through the NodeList and remove the class from each element
+  elementsToRemove.forEach(function(removedElement) {
+      removedElement.classList.remove(MOUSE_VISITED_CLASSNAME);
+  });
+}
 
 // Previous dom, that we want to track, so we can remove the previous styling.
-var mouseDown = false;
+//var mouseDown = false;
 
 document.addEventListener('mousedown', () => {
   mouseDown = true;
@@ -12,54 +38,44 @@ document.addEventListener('mousedown', () => {
 
 document.addEventListener('mouseup', () => {
 // Get the selected text range or create a range manually
+
 var selection = window.getSelection();
 var range = selection.getRangeAt(0);
+range.setStartBefore(range.startContainer);
+range.setEndAfter(range.endContainer);
+// Create a DocumentFragment to hold the contents of the range
+var fragment = range.cloneContents();
 
-// Create a TreeWalker to traverse only <span> elements within the range
-var treeWalker = document.createTreeWalker(
-  range.commonAncestorContainer,
-  NodeFilter.SHOW_ELEMENT, // Show only elements
-  function(node) {
-    return node.tagName.toLowerCase() === 'span' ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_SKIP;
-  },
-  false
-);
+// Create a temporary div to append the fragment and traverse its contents
+var tempDiv = document.createElement("div");
+tempDiv.appendChild(fragment);
 
-// Iterate through the <span> elements within the range and print their content
-while (treeWalker.nextNode()) {
-  var span = treeWalker.currentNode;
-  if (range.intersectsNode(span)) {
+// Find all the <span> elements within the temporary div
+var spanElements = tempDiv.querySelectorAll("span");
+
+// Iterate through the <span> elements and print their content
+spanElements.forEach(function(span) {
     console.log(span.textContent);
-  }
-}
+    span.classList.add(MOUSE_VISITED_CLASSNAME);
+});
 });
 
 document.addEventListener('click', () => {
+  
   mouseDown = false;
   console.log("mousedown is now false")
-});
+}); 
 
 // Mouse listener for any move event on the current document.
 document.addEventListener('mousemove', function (e) {
-  if (mouseDown) {
-    return;
-  }
   var srcElement = e.target;
-  srcElement.onmouseover = e => {
-    if (e.target.nodeName == 'P') {
-      e.target.innerHTML = e.target.innerText.replace(/([\w]+)/g, '<span>$1</span>');
-    }
-  };    
-  //console.log('Text content:', childNodes[i].textContent.trim());
-  
+
   // Lets check if our underlying element is a DIV.
-  if (srcElement.nodeName == 'SPAN' && !mouseDown) {
+  if (srcElement.nodeName == 'SPAN' /*&& !mouseDown*/) {
+    if (PREV_ELEMENT != null) {
+      clearClass();
+    }
     srcElement.classList.add(MOUSE_VISITED_CLASSNAME);
-  } else {
-    var elementsToRemove = document.querySelectorAll(MOUSE_VISITED_CLASSNAME);
-   // Iterate through the selected elements and remove them
-    elementsToRemove.forEach(function(element) {
-      element.parentNode.removeChild(element);
-    });
-  }
+    PREV_ELEMENT = srcElement;
+  } 
 }, false);
